d9e3dd10dd3cb57042f1cfe3d6d45e3e951b61ce
Taumer Anabtawi <tanabtawi147@gmail.com>
2015-07-24 22:29:40 -0400

    added further documentation and cleaned up motion libraries of serial commands (to not interfere with GSM)

--- Source/Energia Source/Motion_Testing/Motion.cpp
+++ Source/Energia Source/Motion_Testing/Motion.cpp
@@ -1,7 +1,7 @@
 //*
 //*  Energia Library for the MPU6050 Accelerometer
 //* 
-//*  Partially based off of: "MPU-6050 Short Example Sketch"
+//*  getAllMotionData() based off of: "MPU-6050 Short Example Sketch"
 //*  By Arduino User JohnChi, August 17, 2014
 //*
 //*  Written by SpEcTrUm
@@ -16,22 +16,47 @@
 #include <Wire.h>
 #include "Motion.h"
 
-const int interrupt_pin = A0;
+const int button_pin = PUSH1;
+const int MPU6050_power = 24;
 
 int count,lock_out = 0;
+
+unsigned long start_time = 0;
+
+bool checking = true;
+bool theft = false;
+bool stopped = false;
+
+/*
+ * Constructor
+ */
 
 motion::motion()
 {
   accX,accY,accZ,tmp,rotX,rotY,rotZ = 0;
-  _interrupt_pin = interrupt_pin;
+  _button_pin = button_pin;
+  _MPU6050_power = MPU6050_power;
 }
+
+/*
+ * Run this once during void setup()
+ * NOTE: do not enable Serial debugging if you are also
+ * using the GSM module
+ */
 
 void motion::setupMPU6050()
 {
+  digitalWrite(_MPU6050_power, LOW);
   Serial.begin(DEBUG_BAUD); // DEBUGGING ONLY
-  pinMode(_interrupt_pin, INPUT);
+  pinMode(_button_pin, INPUT_PULLUP);
+  pinMode(_MPU6050_power, OUTPUT);
   Wire.begin();
 }
+
+/* 
+ * Added for flexibility, unused in mainProgram.ino
+ * Sets up the MPU6050 for normal accelerometer/gyro readings
+ */
 
 void motion::runDefaultSetup()
 {
@@ -45,15 +70,36 @@ void motion::runDefaultSetup()
   Wire.endTransmission();
 }
 
+/*
+ * This runs before the theft detection logic in mainProgram.ino
+ * Enables the MPU6050 onboard "motion detection interrupt"
+ *
+ * The "motion interrupt" will trigger a result in the INT_STATUS register
+ * of the MPU6050 if the combined motion surpasses the value of THEFT_THRESHOLD 
+ * defined in Motion.h
+ *
+ * The function is simply several Wire.writes that set the required
+ * MPU6050 registers as per the InvenSense documentation
+ *
+ * For readabilty, most of the writes refer to the regiter via #defines 
+ * found in Motion.h
+ */
+
 void motion::runTheftSetup()
 {
+  resetMPU6050(); // toggles power to the MPU6050, required for accurate writes to the registers
+
   int temp = 0;
   count = 0;
+
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(PWR_MGMT_1);
   Wire.write(0);
   Wire.endTransmission();
+
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(PWR_MGMT_2);
@@ -65,6 +111,8 @@ void motion::runTheftSetup()
   Wire.write(temp);  // write 0 to bits [5:3]
   Wire.endTransmission();
 
+  delay(10);
+
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(ACCEL_CONFIG);
   Wire.endTransmission(false);
@@ -74,6 +122,8 @@ void motion::runTheftSetup()
   Wire.write(ACCEL_CONFIG);
   Wire.write(temp);  // write 0 to bits [2:0]
   Wire.endTransmission();
+  
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(CONFIG);
@@ -85,22 +135,35 @@ void motion::runTheftSetup()
   Wire.write(temp);  // write 0 to bits [2:0]
   Wire.endTransmission();
 
+  delay(10);
+
+  Wire.beginTransmission(MPU_ADDRESS);
+  Wire.write(INT_ENABLE);
+  Wire.write(0);
+  Wire.endTransmission();
+
+  delay(10);
+
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(INT_ENABLE);
   Wire.write(MOT_EN);
   Wire.endTransmission();
+
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(MOT_DUR);
   Wire.write(DEFAULT_DURATION);
   Wire.endTransmission();
 
+  delay(10);
+
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(MOT_THR);
   Wire.write(THEFT_THRESHOLD);
   Wire.endTransmission();
 
-  delay(100);
+  delay(50);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(ACCEL_CONFIG);
@@ -112,6 +175,8 @@ void motion::runTheftSetup()
   Wire.write(temp + ACCEL_HPF_HIGH);  // write B111 to bits [2:0]
   Wire.endTransmission();
 
+  delay(10);
+
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(PWR_MGMT_2);
   Wire.endTransmission(false);
@@ -121,6 +186,10 @@ void motion::runTheftSetup()
   Wire.write(PWR_MGMT_2);
   Wire.write(temp + WKUP_GYR_OFF);  // write B01 to bits [7:6] (frequency of 5Hz) and B111 to bits [2:0] (sleeps gyro)
   Wire.endTransmission();
+
+  delay(10);
+
+  // Below enables power cycling of the MPU6050 at 5 Hz (saves some battery life)
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(PWR_MGMT_1);
@@ -134,15 +203,41 @@ void motion::runTheftSetup()
 
 }
 
+/*
+ * This runs before the crash and stop detection logic in mainProgram.ino
+ * Enables the MPU6050 onboard "motion detection interrupt" and the onboard 
+ * "zero-motion interrupt"
+ *
+ * The "motion interrupt" will trigger a result in the INT_STATUS register
+ * of the MPU6050 if the combined motion surpasses the value of MOTION_THRESHOLD 
+ * defined in Motion.h
+ *
+ * The "zero-motion interrupt" will trigger a result in the MOT_DETECT_STATUS register of 
+ * the MPU6050 if the combined motion surpasses the value of ZMOTION_THRESHOLD defined 
+ * in Motion.h and another flag when no motion is initially detected.
+ *
+ * The function is simply several Wire.writes that set the required
+ * MPU6050 registers as per the InvenSense documentation
+ *
+ * For readabilty, most of the writes refer to the regiter via #defines 
+ * found in Motion.h
+ */
+
 void motion::runSafetySetup()
 {
+  resetMPU6050();
   int temp = 0;
+  count = 0;
   lock_out = 0;
+
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(PWR_MGMT_1);
   Wire.write(0);
   Wire.endTransmission();
+
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(PWR_MGMT_2);
@@ -153,6 +248,8 @@ void motion::runSafetySetup()
   Wire.write(PWR_MGMT_2);
   Wire.write(temp);  // write 0 to bits [5:3]
   Wire.endTransmission();
+  
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(ACCEL_CONFIG);
@@ -163,6 +260,8 @@ void motion::runSafetySetup()
   Wire.write(ACCEL_CONFIG);
   Wire.write(temp);  // write 0 to bits [2:0]
   Wire.endTransmission();
+  
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(CONFIG);
@@ -173,23 +272,50 @@ void motion::runSafetySetup()
   Wire.write(CONFIG);
   Wire.write(temp);  // write 0 to bits [2:0]
   Wire.endTransmission();
+  
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(INT_ENABLE);
-  Wire.write(MOT_EN);
+  Wire.write(0);
   Wire.endTransmission();
+  
+  delay(10);
+
+  Wire.beginTransmission(MPU_ADDRESS);
+  Wire.write(INT_ENABLE);
+  Wire.write(ZRMOT_EN + MOT_EN);
+  Wire.endTransmission();
+  
+  delay(10);
+
+  Wire.beginTransmission(MPU_ADDRESS);
+  Wire.write(ZRMOT_DUR);
+  Wire.write(DEFAULT_DURATION);
+  Wire.endTransmission();
+
+  delay(10);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(MOT_DUR);
   Wire.write(DEFAULT_DURATION);
   Wire.endTransmission();
 
+  delay(10);
+
+  Wire.beginTransmission(MPU_ADDRESS);
+  Wire.write(ZRMOT_THR);
+  Wire.write(ZRMOTION_THRESHOLD);
+  Wire.endTransmission();
+  
+  delay(50);
+
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(MOT_THR);
-  Wire.write(MOTION_THRESHOLD);
+  Wire.write(THEFT_THRESHOLD);
   Wire.endTransmission();
 
-  delay(100);
+  delay(50);
 
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(ACCEL_CONFIG);
@@ -201,6 +327,8 @@ void motion::runSafetySetup()
   Wire.write(temp + ACCEL_HPF_HIGH);  // write B111 to bits [2:0]
   Wire.endTransmission();
 
+  delay(10);
+
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(PWR_MGMT_2);
   Wire.endTransmission(false);
@@ -208,9 +336,31 @@ void motion::runSafetySetup()
   temp = Wire.read() & WKUP_FREQ;  // only bits [7:6]
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(PWR_MGMT_2);
-  Wire.write(temp + WKUP_GYR_ON);  // write B01 to bits [7:6] (frequency of 5Hz) and B000 to bits [2:0] (enables gyro)
+  Wire.write(temp + WKUP_GYR_OFF);  // write B01 to bits [7:6] (frequency of 5Hz) and B111 to bits [2:0] (disables gyro)
   Wire.endTransmission();
+
+  // Unlike runTheftSetup(), we do not wish to cycle the MPU6050 during crash detection
 }
+
+/*
+ * Small function to fix a hardware quirk of the MPU6050
+ *
+ * Controls a PNP transistor which toggles the power before 
+ * actually writing to the MPU6050 registers (acts like a hard-reset)
+ */
+
+void motion::resetMPU6050()
+{
+  digitalWrite(_MPU6050_power, HIGH);
+  delay(100);
+  digitalWrite(_MPU6050_power, LOW);
+}
+
+/* 
+ * Added for flexibility, unused in mainProgram.ino
+ * Retrieves all MPU6050 motion data averaged over 
+ * the #defined sample size SAMPLES
+ */
 
 void motion::getAllMotionData()
 {
@@ -236,7 +386,21 @@ void motion::getAllMotionData()
   rotX = rX/SAMPLES;
   rotY = rY/SAMPLES;
   rotZ = rZ/SAMPLES;
+
+  //Serial.print("AcX = "); Serial.print(accX);
+  //Serial.print(" | AcY = "); Serial.print(accY);
+  //Serial.print(" | AcZ = "); Serial.print(accZ);
+  //Serial.print(" | Tmp = "); Serial.print(wingman.temp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  //Serial.print(" | GyX = "); Serial.print(rotX);
+  //Serial.print(" | GyY = "); Serial.print(rotY);
+  //Serial.print(" | GyZ = "); Serial.println(rotZ);
+  //delay(1000);
 }
+
+/*
+ * Retrieves the MPU6050 accelerometer data averaged over 
+ * the #defined sample size SAMPLES
+ */
 
 void motion::getAccData()
 {
@@ -247,7 +411,7 @@ void motion::getAccData()
     Wire.beginTransmission(MPU_ADDRESS);
     Wire.write(ACCEL_XOUT_H);  // starting with register 0x3B (ACCEL_XOUT_H)
     Wire.endTransmission(false);
-    Wire.requestFrom(MPU_ADDRESS,6,true);  // request a total of 14 registers
+    Wire.requestFrom(MPU_ADDRESS,6,true);  // request a total of 6 registers
     aX += Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
     aY += Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
     aZ += Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
@@ -256,6 +420,12 @@ void motion::getAccData()
   accY = aY/SAMPLES;
   accZ = aZ/SAMPLES; 
 }
+
+/* 
+ * Added for flexibility, unused in mainProgram.ino
+ * Retrieves the MPU6050 gyro data averaged over 
+ * the #defined sample size SAMPLES
+ */
 
 void motion::getRotData()
 {
@@ -276,39 +446,9 @@ void motion::getRotData()
   rotZ = rZ/SAMPLES; 
 }
 
-void motion::getCrashData()
-{
-  int currX,currY,currZ,prevX,prevY,prevZ = 0;
-  int cX,cY,cZ,pX,pY,pZ = 0;
-  for(int i = 0; i < SAMPLES/2; i++)
-  {
-    getRotData();
-    pX += rotX;
-    pY += rotY;
-    pZ += rotZ;
-    delay(100);
-    getRotData();
-    cX += rotX;
-    cY += rotY;
-    cZ += rotZ;
-  }
-  prevX =   (pX/SAMPLES)/2;
-  prevY =   (pY/SAMPLES)/2;
-  prevZ =   (pZ/SAMPLES)/2;
-  currX =   (cX/SAMPLES)/2;
-  currY =   (cY/SAMPLES)/2;
-  currZ =   (cZ/SAMPLES)/2;
-
-  Serial.print(" | GyXDiff = "); Serial.print(currX-prevX);
-  Serial.print(" | GyYDiff = "); Serial.print(currY-prevY);
-  Serial.print(" | GyZDiff = "); Serial.println(currZ-prevZ);
-}
-
-void motion::interruptDebug()
-{
-  Serial.println(digitalRead(_interrupt_pin));
-  delay(100);
-}
+/*
+ * Function to enable the SafT Brake Light via the air-booster pack
+ */
 
 void motion::brakeLight(bool signal)
 {
@@ -319,47 +459,182 @@ void motion::brakeLight(bool signal)
   }
   else
   {
+    count = 0;
     digitalWrite(RED_LED, LOW);
   }
 }
+
+/*
+ * Function to enable the SafT Emegency Lights via the air-booster pack
+ */
 
 void motion::emergencyFlash()
 {
   // placeholder -- in final code, will be air-pack commands
   digitalWrite(RED_LED, LOW);
   digitalWrite(BLUE_LED, HIGH);
+  delay(500);
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(BLUE_LED, LOW);
+  delay(500);
 }
 
-bool motion::motionDetected()
+/*
+ * Function that detects a stop event based on several bool functions
+ */
+
+void motion::checkForStop()
 {
+  if(!stopped && stopDetected())
+    {
+      brakeLight(ON);
+      stopped = true;
+    }
+  if(stopped)
+  {
+    if(motionDetected())
+    {
+      brakeLight(OFF);
+      stopped = false;
+    }
+  }
+}
+
+/*
+ * Bool function that checks for a motion-event logged by the MPU6050
+ * "motion interrupt"
+ */
+
+bool motion::theftDetected()
+{
+  bool theft = false;
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(INT_STATUS);
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_ADDRESS,1,true);
   int temp = Wire.read();
-  if(temp == MOTION_DETECTED)
+  //Serial.print("INT_STATUS: ");Serial.print(temp, HEX); Serial.print(" | "); Serial.println(temp, BIN);
+  if(temp == THEFT_DETECTED)
+  {
+    count++;
+    if(count > 5)
+    {
+      theft = true;
+    }
+  }
+  return theft;
+}
+
+/*
+ * Bool function that checks for a motion-event logged by the MPU6050
+ * "zero-motion interrupt"
+ */
+
+bool motion::motionDetected()
+{
+  bool motion = false;
+  Wire.beginTransmission(MPU_ADDRESS);
+  Wire.write(MOT_DETECT_STATUS);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDRESS,1,true);
+  int temp = Wire.read();
+ // Serial.print("MOT_DETECT_STATUS: ");Serial.print(temp, HEX); Serial.print(" | "); Serial.println(temp, BIN);
+  if(temp >= MOTION_DETECTED)
+  {
+    count++;
+    if(count > 2)
+    {
+      motion = true;
+    }
+  }
+  return motion;
+}
+
+/*
+ * Bool function that checks for a based on several values and a unique
+ * qualification period #defined in Motion.h
+ */
+
+bool motion::stopDetected()
+{
+  Wire.beginTransmission(MPU_ADDRESS);
+  Wire.write(MOT_DETECT_STATUS);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDRESS,1,true);
+  int temp = Wire.read();
+  //Serial.print("MOT_DETECT_STATUS: "); Serial.print(temp, HEX); Serial.print(" | "); Serial.println(temp);
+  //Serial.print("count: "); Serial.println(count);
+  if(count == 0 && temp == STOP_DETECTED)
   {
     count++;
   }
-  if(count > 1 && temp == MOTION_DETECTED)
-  {
-    return true;
-  }
   else
   {
-    return false;
+    if(count == 1 && temp == STOP_DETECTED)
+    {
+      //Serial.println("First Stop");
+      //Serial.println(checking);
+      if(checking)
+      {
+        start_time = millis();
+        checking = false;
+      }
+      if(millis() - start_time > STOP_QUALIFICATION)
+      {
+        //Serial.println(start_time);
+        checking = true;
+        Wire.beginTransmission(MPU_ADDRESS);
+        Wire.write(MOT_DETECT_STATUS);
+        Wire.endTransmission(false);
+        Wire.requestFrom(MPU_ADDRESS,1,true);
+        //Serial.println("Second Stop");
+        if(Wire.read() == STOP_DETECTED)
+        {       
+          return true;
+        }
+      }
+    }
   }
+  return false;
 }
 
-bool motion::crashDetected()
+/*
+ * Function that calculates the pitch and roll of The Wingman and determines wether or not an actual crash
+ * has occured
+ *
+ * Several crash detection techniques were analyzed, the best of which is used below
+ */
+
+bool motion::crashDetected() // void --> debugging, bool --> correct/release mode
 {
+  double pitch,roll = 0;
+ /// if(digitalRead(_button_pin) == LOW) {
+  ///for(int i = 0; i < 1000; i++)
+  ///{
+  int X,Y,Z = 0;
+  getAccData();
+  X = accX;
+  Y = accY;
+  Z = accZ;
+  double temp = pow((pow(Y,2) + pow(Z,2)),0.5);
+  roll  = (atan2(-Y, Z)*180.0)/PI;
+  pitch = (atan2(X, temp)*180.0)/PI;
+  if(abs(pitch) <= 60)
+  {
+    pitch = 0;
+  }
+  if(abs(roll) <= 60)
+  {
+    roll = 0;
+  } 
+
   if(lock_out >= 1)
   {
     return true;
   }
   else
   {
-    if(abs(rotZ) >= ROLLOVER && abs(rotX) >= ROLLOVER)
+    if((abs(pitch) + abs(roll)) >= 150)
     {
       lock_out++;
       return true;
@@ -369,4 +644,12 @@ bool motion::crashDetected()
       return false;
     }
   }
-}
+   /* Serial.print(abs(pitch) + abs(roll)); Serial.print(",");
+    delay(100);
+  }
+    Serial.println(" "); Serial.println("All Done!");
+    while(1)
+    {
+        
+    }
+}*/}
--- Source/Energia Source/Motion_Testing/Motion.h
+++ Source/Energia Source/Motion_Testing/Motion.h
@@ -15,76 +15,84 @@
 #ifndef _MSP432_MOTION_
 #define _MSP432_MOTION_
 
-#define MPU_ADDRESS 0x68       // I2C address of the MPU-6050
+#define MPU_ADDRESS      	0x68      // I2C address of the MPU-6050
 
-// MPU6050 Registers:
-#define PWR_MGMT_1 0x6B        // PWR_MGMT_1 register
-#define PWR_MGMT_2 0x6C        // PWR_MGMT_2 register
-#define INT_ENABLE 0x38        // MPU6050 interrupt enable 
-#define INT_STATUS 0x3A        // MPU6050 interrupt status
-#define CONFIG 0x1A		       // MPU6050 LPF settings
-#define ACCEL_CONFIG 0x1C      // configures MPU6050 accelerometer
-#define MOT_EN 0x40            // enables MPU6050 "Motion Detection" to generate an interrupt
-#define MOT_DUR 0x20		   // sets duration for MPU6050 "Motion Detection"
-#define MOT_THR 0x1F           // sets threshold for MPU6050 "Motion Detection"
+#define PWR_MGMT_1       	0x6B      // PWR_MGMT_1 register
+#define PWR_MGMT_2       	0x6C      // PWR_MGMT_2 register
+#define INT_ENABLE       	0x38      // MPU6050 interrupt enable 
+#define INT_STATUS       	0x3A      // MPU6050 interrupt status
+#define MOT_DETECT_STATUS       0x61      // MPU6050 "Zero-motion" interrupt status
+#define CONFIG           	0x1A	  // MPU6050 LPF settings
+#define ACCEL_CONFIG     	0x1C      // configures MPU6050 accelerometer
+#define MOT_EN           	0x40      // enables MPU6050 "Motion Detection" to generate an interrupt
+#define MOT_DUR          	0x20	  // sets duration for MPU6050 "Motion Detection"
+#define MOT_THR          	0x1F      // sets threshold for MPU6050 "Motion Detection"
+#define ZRMOT_EN         	0x20
+#define ZRMOT_DUR        	0x22 
+#define ZRMOT_THR        	0x21 
 
-#define ACCEL_XOUT_H 0x3B      // x-direction register -- bits [15:8]
-#define ACCEL_XOUT_L 0x3C      // x-direction register -- bits [7:0] 
-#define ACCEL_YOUT_H 0x3D      // y-direction register -- bits [15:8]
-#define ACCEL_YOUT_L 0x3E      // y-direction register -- bits [7:0]
-#define ACCEL_ZOUT_H 0x3F      // z-direction register -- bits [15:8]
-#define ACCEL_ZOUT_L 0x40      // z-direction register -- bits [7:0]
-#define TEMP_OUT_H 0x41        // temperature register -- bits [15:8]
-#define TEMP_OUT_L 0x42        // temperature register -- bits [7:0]
-#define GYRO_XOUT_H 0x43       // x-direction register -- bits [15:8]
-#define GYRO_XOUT_L 0x44       // x-direction register -- bits [7:0]
-#define GYRO_YOUT_H 0x45       // y-direction register -- bits [15:8]
-#define GYRO_YOUT_L 0x46       // y-direction register -- bits [7:0]
-#define GYRO_ZOUT_H 0x47       // z-direction register -- bits [15:8]
-#define GYRO_ZOUT_L 0x48       // z-direction register -- bits [7:0]
+#define ACCEL_XOUT_H     	0x3B      // x-direction register -- bits [15:8]
+#define ACCEL_XOUT_L     	0x3C      // x-direction register -- bits [7:0] 
+#define ACCEL_YOUT_H     	0x3D      // y-direction register -- bits [15:8]
+#define ACCEL_YOUT_L     	0x3E      // y-direction register -- bits [7:0]
+#define ACCEL_ZOUT_H     	0x3F      // z-direction register -- bits [15:8]
+#define ACCEL_ZOUT_L     	0x40      // z-direction register -- bits [7:0]
+#define TEMP_OUT_H       	0x41      // temperature register -- bits [15:8]
+#define TEMP_OUT_L       	0x42      // temperature register -- bits [7:0]
+#define GYRO_XOUT_H      	0x43      // x-direction register -- bits [15:8]
+#define GYRO_XOUT_L      	0x44      // x-direction register -- bits [7:0]
+#define GYRO_YOUT_H      	0x45      // y-direction register -- bits [15:8]
+#define GYRO_YOUT_L      	0x46      // y-direction register -- bits [7:0]
+#define GYRO_ZOUT_H      	0x47      // z-direction register -- bits [15:8]
+#define GYRO_ZOUT_L      	0x48      // z-direction register -- bits [7:0]
 
-#define STDBY_ACCEL 0xC7	   // used to set bits [5:3] in the PWR_MGMT_2 register
-#define ACCEL_HPF 0xF8         // used to set bits [2:0] in the ACCEL_CONFIG register (high-pass filter)
-#define ACCEL_HPF_HIGH 7       // used to set bits [2:0] in the ACCEL_CONFIG register (high-pass filter)
-#define DLPF_CFG 0xF8		   // used to set bits [2:0] in the CONFIG register (high-pass filter bandwidth)
-#define WKUP_FREQ 0x3F         // used to set wakeup frequency of 5Hz (PWR_MGMT_2 register)
-#define SET_CYCLE 0x9F		   // isolates PWR_MGMT_1 bits [6:5]
-#define CYCLE_ENABLE 0x10      // enables cycling of MPU6050 (disables sleep)
+#define STDBY_ACCEL      	0xC7      // used to set bits [5:3] in the PWR_MGMT_2 register
+#define ACCEL_HPF        	0xF8      // used to set bits [2:0] in the ACCEL_CONFIG register (high-pass filter)
+#define ACCEL_HPF_HIGH   	7         // used to set bits [2:0] in the ACCEL_CONFIG register (high-pass filter)
+#define DLPF_CFG         	0xF8	  // used to set bits [2:0] in the CONFIG register (high-pass filter bandwidth)
+#define WKUP_FREQ        	0x3F      // used to set wakeup frequency of 5Hz (PWR_MGMT_2 register)
+#define SET_CYCLE        	0x9F      // isolates PWR_MGMT_1 bits [6:5]
+#define CYCLE_ENABLE     	0x10      // enables cycling of MPU6050 (disables sleep)
 
-#define DEFAULT_DURATION 1
-#define THEFT_THRESHOLD 40     // threshold for MPU6050 "Motion Detection" during theft
-#define MOTION_THRESHOLD 20    // threshold for MPU6050 "Motion Detection" during a crash
-#define MOTION_DETECTED 0x41   // if the INT_STATUS register returns 0x41 theft has occured
-#define WKUP_GYR_OFF 0x47      // sets PWR_MGMT_2 resgister to desired refresh rate (gyro DISABLED)
-#define WKUP_GYR_ON 0x40       // sets PWR_MGMT_2 resgister to desired refresh rate (gyro ENABLED)
+#define DEFAULT_DURATION 	1
+#define THEFT_THRESHOLD  	20        // threshold for MPU6050 "Motion Detection" during theft
+#define ZRMOTION_THRESHOLD 	60        // threshold for MPU6050 "Zero-motion Detection" during a crash
+#define THEFT_DETECTED  	0x41      // if the INT_STATUS register returns 0x41 theft has occured
+#define MOTION_DETECTED     0x20      // 
+#define STOP_DETECTED       0
+#define WKUP_GYR_OFF     	0x47      // sets PWR_MGMT_2 resgister to desired refresh rate (gyro DISABLED)
+#define WKUP_GYR_ON      	0x40      // sets PWR_MGMT_2 resgister to desired refresh rate (gyro ENABLED)
 
-#define SAMPLES 10             // defines the number of samples for averaging within getMotionData()
-#define DEBUG_BAUD 9600        // baud-rate for debugging (9600 works best)
-#define READING_DELAY 100      // delay-rate for MPU6050 readings (in ms)
-#define ROLLOVER 4000		   // value that deterimines a "crash event" (based on X and Z gyro values)
-#define ON true                // used for brake light functionality
-#define OFF false              // used for brake light functionality
+#define SAMPLES          	10        // defines the number of samples for averaging within getMotionData()
+#define DEBUG_BAUD       	9600      // baud-rate for debugging (9600 works best)
+#define READING_DELAY    	100       // delay-rate for MPU6050 readings (in ms)
+#define STOP_QUALIFICATION  2000
+#define ON               	true      // used for brake light functionality
+#define OFF              	false     // used for brake light functionality
 
 class motion
 {
     public:
 	    motion();
-		void setupMPU6050(void);    	       // begins Wire communication and serial debugging
-		void runDefaultSetup(void); 	       // initializes the MPU6050 for normal readings (always on)
-		void runTheftSetup(void);   	       // initializes the MPU6050 "Motion Detection" (cycles as defined in THEFT_CYCLE_RATE)
-		void runSafetySetup(void);  	       // initializes the MPU6050 "Motion Detection" and gyrometer readings (non-cycle mode)
+		void setupMPU6050(void);    	      // begins Wire communication and serial debugging
+		void runDefaultSetup(void); 	      // initializes the MPU6050 for normal readings (always on)
+		void runTheftSetup(void);   	      // initializes the MPU6050 "Motion Detection" (cycles as defined in THEFT_CYCLE_RATE)
+		void runSafetySetup(void);  	      // initializes the MPU6050 "Motion Detection" and gyrometer readings (non-cycle mode)
+		void resetMPU6050(void);
 		void getAllMotionData(void);
 		void getAccData(void);
 		void getRotData(void);
-		void getCrashData(void);
-		void interruptDebug(void);
-		void brakeLight(bool signal);				   // signals MSP430 to activate brake light 			
-		void emergencyFlash(void); 			   // used in event of a crash, tells MSP430 to flash all 3 channels constantly
-		bool motionDetected(void);             // returns true if motion is occuring -- threshold set by either theft or safety setup	
-		bool crashDetected(void);              // returns TRUE if a crash has been detected
+		void brakeLight(bool signal);	      // signals MSP430 to activate brake light 			
+		void emergencyFlash(void); 	          // used in event of a crash, tells MSP430 to flash all 3 channels constantly
+		void checkForStop(void);
+                bool theftDetected(void);
+		bool motionDetected(void);
+		bool stopDetected(void);
+		bool crashDetected(void);             // returns TRUE if a crash has been detected
 		int accX,accY,accZ,tmp,rotX,rotY,rotZ;
 	private:
-		int _interrupt_pin;			
+		int _button_pin;
+		int _MPU6050_power;			
 }; 
 
 #endif
